072f036f8a06eab8070ed66f593fe42f7ab19189
BeataMaro <beatamaro@interia.pl>
2021-03-15 01:20:34 +0100

    Home Page

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { DiscountBar } from "./components/DiscountBar";
 import { Header } from "./components/Header";
 import { NavBar } from "./components/NavBar";
 import { Breadcrumbs } from "./components/Breadcrumbs";
-// import { Home } from "./components/Home";
+import { Home } from "./components/Home";
 import { Products } from "./components/Products";
 import { Cart } from "./components/Cart";
 import { Account } from "./components/Account";
@@ -27,11 +27,11 @@ function App() {
             <Route path='/products' component={Products} />
             <Route path='/account' component={Account} />
             <Route path='/cart' component={Cart} />
-            {/* <Route exact path='/' component={Home} /> */}
+            <Route exact path='/' component={Home} />
           </Switch>
           <NavBar />
           <Breadcrumbs />
-          {/* <Home /> */}
+          <Home />
           <Products />
           <Footer />
         </BrowserRouter>
