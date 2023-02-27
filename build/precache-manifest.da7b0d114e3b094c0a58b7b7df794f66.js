80968e768f23a511d81c4e355032c42b7f5783fa
BeataMaro <beatamaro@interia.pl>
2023-02-27 20:26:28 +0100

    send production version

---
+++ build/precache-manifest.da7b0d114e3b094c0a58b7b7df794f66.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1c50c42fa201a298347eb00454e2ff0",
+    "url": "/your-shop/public/index.html"
+  },
+  {
+    "revision": "4ccf35d30f29513617df",
+    "url": "/your-shop/public/static/css/main.ae37d1f8.chunk.css"
+  },
+  {
+    "revision": "e84383b45dc688c447e8",
+    "url": "/your-shop/public/static/js/2.cc0c453d.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/your-shop/public/static/js/2.cc0c453d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ccf35d30f29513617df",
+    "url": "/your-shop/public/static/js/main.20eee9a7.chunk.js"
+  },
+  {
+    "revision": "9828e3b313982c055504",
+    "url": "/your-shop/public/static/js/runtime-main.6ee5c459.js"
+  },
+  {
+    "revision": "cc5049391ec037c6af7033f4558048c0",
+    "url": "/your-shop/public/static/media/mom-black-jeans2.cc504939.jpg"
+  },
+  {
+    "revision": "f716771789ecb9e6bbc09a6e934d4b33",
+    "url": "/your-shop/public/static/media/nordgreen-unsplash.f7167717.jpg"
+  },
+  {
+    "revision": "b5d75e05d0c7de9e8b59c6493ac25bcd",
+    "url": "/your-shop/public/static/media/skirt.b5d75e05.jpg"
+  },
+  {
+    "revision": "075972ff7ba533221e84284bfaa026c3",
+    "url": "/your-shop/public/static/media/wesley-tingey-unsplash.075972ff.jpg"
+  },
+  {
+    "revision": "2801fa0a5bfc9285f1e8bce3b3f068ab",
+    "url": "/your-shop/public/static/media/white-sweater.2801fa0a.jpg"
+  }
+]);
